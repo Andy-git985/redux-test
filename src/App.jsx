61a61b9b36190a2d3ee5322fc71d3f8b96719680
Andy-git985 <andy.yu617@gmail.com>
2023-03-29 19:00:48 -0400

    move store wrapper to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import { extendedApiSlice } from './features/schedule/scheduleSlice';
 import Schedule from './features/schedule/Schedule';
 import Employee from './features/user/Employee';
 
-store.dispatch(extendedApiSlice.endpoints.getSchedule.initiate());
-
 function App() {
-  return (
-    <Provider store={store}>
-      <Schedule />
-    </Provider>
-  );
+  return <Schedule />;
 }
 
 export default App;
